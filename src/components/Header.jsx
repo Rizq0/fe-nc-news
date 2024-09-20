5451c58ff61a4362dd10a137bdb5c96e37198af3
Joe <joe_ethan2@hotmail.com>
2024-09-20 14:22:59 +0100

    add: header now as context to logged in user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,10 @@ export const Header = () => {
       </Link>
       <div className="vertical-divider"></div>
       <h1 className="headertitle">NC-NEWS</h1>
-      <h1 className="username">{userName}</h1>
+      <div className="user-area">
+        <p className="logged-in-as">Logged In As:</p>
+        <h1 className="username">{userName}</h1>
+      </div>
     </header>
   );
 };
